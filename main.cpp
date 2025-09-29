92d8264eb620f663acec058d93d2f85b314cb34f
Prajwal Patil <prajwalspatil4@gmail.com>
2025-09-29 18:57:14 +0530

    Added helper functions

--- main.cpp
+++ main.cpp
@@ -1,17 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std ;
-
-struct piece{
+typedef struct piece{
     string source;
     int start;
     int length;
-};
+}piece;
+class pieceTable{
+    
+    private:
+        vector<piece> Pieces;
+        int state=0;
+        piece current_piece=NULL;
+        string add="";
+        string original="";
 
+    public:
+        int nextIndex(int index){
+            int originalIndex=index;
+            int i=0;
+            if(index==0)return 0;
+            for(auto &it:Pieces){
+                i++;
+                index-=it.length;
+                if(index==0){
+                    return i;
+                }
+                if(index<0){
+                    int n=it.start;
+                    int len=it.length;
+                    it.length=originalIndex-n;
+                    piece newPiece=new piece;
+                    newPiece.source="add";
+                    newPiece.start=originalIndex;
+                    newPiece.length=len+n-originalIndex;
+                    Pieces.insert(i,newPiece);
+                    return i;
+                }
+            }
+            return Pieces.length();
+        }
+        void insert(char c, int index){
+            if(state==0){
+                current_piece.source = "add";
+                current_piece.start = add.length();
+                current_piece.length = 1;
+
+                add.push_back(c);
+                Pieces.insert(Pieces.begin()+k , current_piece);
+                state = 1;
+            }
+
+            else if(state == 1){
+                current_piece.length++;
+                add.push_back(c);
+            }
+        }
+};
 int main(){
-    vector<piece> Pieces;
-    int state;
-    piece current_piece;
-    string add;
+    
     char c; //decided from keyboard
     int k; //decided from cursor
 
@@ -20,20 +66,7 @@ int main(){
 
     //insertion
 
-    if(state==0){
-        current_piece.source = add;
-        current_piece.start = add.length();
-        current_piece.length = 1;
-
-        add.push_back(c);
-        Pieces.insert(Pieces.begin()+k , current_piece);
-        state = 1;
-    }
-
-    else if(state == 1){
-        current_piece.length++;
-        add.push_back(c);
-    }
+    
     
 
     //deletion
